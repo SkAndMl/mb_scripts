2f87975fcca6f4d81b2de2b5b771b54c5c84d25a
Sathya Krishnan <86184014+SathyaKrishnan1211@users.noreply.github.com>
2022-04-25 18:20:12 +0530

    setup and license

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+VERSION = '0.0.7'
+DESCRIPTION = 'Machine Learning scripts that will quicken the modelling and data analysis process'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="mb_scripts",
+    version=VERSION,
+    author="Sathya Krishnan Suresh",
+    author_email="<satyakrishnan.s@pec.edu>",
+    description=DESCRIPTION,
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        "scikit-learn",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "tensorflow"
+    ],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'mb_scripts','machine learning','data science','data analysis'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
