b366970b343743be500d47a774844be41b80cdf7
Sathya Krishnan <86184014+SathyaKrishnan1211@users.noreply.github.com>
2022-05-28 08:00:44 +0530

    moved plot_importances_rf to feature_selection

--- mb_scripts/plotting.py
+++ mb_scripts/plotting.py
@@ -117,32 +117,3 @@ def train_validation_curve_for_rf(X,y,val_size=0.3,rs=42,epochs=10,n_estimators=
         plt.sca(ax[2])
         plot_2([x for x in range(n_estimators_start,n_estimators_start+epochs)],
                rec_tr,rec_val,'train','val','train/val recall')
-
-
-def plot_importance_rf(df, cols=None, type_of_model="c"):
-    from sklearn import ensemble
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import seaborn as sns
-
-    if cols != None:
-        data = df[cols].copy()
-    else:
-        data = df.copy()
-
-    if type_of_model == "c":
-        rf = ensemble.RandomForestClassifier()
-    else:
-        rf = ensemble.RandomForestRegressor()
-
-    rf = rf.fit(data.iloc[:, :-1], data.iloc[:, -1])
-    imps = rf.feature_importances_
-    idxs = np.argsort(imps)
-    cols = df.columns
-
-    sns.set_style("whitegrid")
-    plt.title("FEATURE IMPORTANCES", size=20)
-    plt.barh(range(len(idxs)), imps[idxs])
-    plt.yticks(range(len(idxs)), [cols[i] for i in idxs])
-    plt.xlabel("RANDOM FOREST IMPORTANCE")
-    plt.show();
