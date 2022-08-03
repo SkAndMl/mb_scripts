7f9c5d08878c617abd70b8bc4eecaa83d3e3588e
Sathya Krishnan <86184014+SathyaKrishnan1211@users.noreply.github.com>
2022-08-03 19:00:20 +0530

    strat kfold update

--- mb_scripts/useful_scripts.py
+++ mb_scripts/useful_scripts.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
-from sklearn import model_selection
+from sklearn import model_selection,metrics,ensemble
 from .metrics import precision_binary,recall_binary,accuracy_score,f1_score
 
 def train_test_split(X,y,test_size=0.2,random_state=42):
@@ -215,5 +215,28 @@ class DataPreparation():
                 raise ValueError(f"Column dimension do not match while one-hot encoding is being performed\n{self.new_cols.columns}\
                                 \n{new_cols.columns}")
             
-        
         return new_data
+def strat_kfold_results(models,X,y,n_splits=5,voting=True):
+    skf = model_selection.StratifiedKFold(n_splits=n_splits,
+                                          shuffle=True,
+                                         random_state=seed)
+    fold=1
+    
+    if voting:
+        vtg_clf = ensemble.VotingClassifier([
+            (f"model_{model.__class__.__name__}",model) for model in models
+        ],voting="soft")
+        models.append(vtg_clf)
+    
+    for train_ind,val_ind in skf.split(X,y):
+        print(f"{'='*20}FOLD:{fold}{'='*20}")
+        x_train,x_val = X[train_ind],X[val_ind]
+        y_train,y_val = y[train_ind],y[val_ind]
+
+        for model in models:
+            model = model.fit(x_train,y_train)
+            pred_proba = model.predict_proba(x_val)[:,1]
+            roc_score = metrics.roc_auc_score(y_val,pred_proba)
+            print(f"MODEL: {model.__class__.__name__} ROC SCORE: {roc_score} ")
+           
+        fold+=1
